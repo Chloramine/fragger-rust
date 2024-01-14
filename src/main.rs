416c399024db5b0827a9850497c6751e009a7d49
Chloramine <40995784+Chloramine@users.noreply.github.com>
2024-01-13 17:12:07 -0800

    i don't even know. command doesn't work will fix later lol

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use std::env;
-use serenity::all::*;
-mod info;
+use serenity::{all::*, framework::standard::{Configuration, macros::group}};
+mod files;
+mod commands;
+
+use crate::commands::misc::ping::*;
 
 struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
         println!("running fragger on {}", ready.user.name);
     }
 }
 
+#[group]
+#[commands(ping)]
+struct General;
+
+
 #[tokio::main]
 async fn main() {
-    let token = info::token(); // store token in env variable so as to not post it to github.
+    let token = files::info::token(); // store token in env variable so as to not post it to github.
     let intents = GatewayIntents::all();
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(
+        Configuration::new().with_whitespace(true)
+            .prefix(files::info::prefix())); // todo: incorporate info::ADMINS into .owners()
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .await
